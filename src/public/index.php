32a68c1a5f8dea3e8fff0eabaca7900560f69a02
mikan19 <mellamo.rina.ty@gmail.com>
2023-04-27 23:03:36 +0900

    fix:45000が出力されるよう修正しました

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,13 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<?php 
+$spendings = ["apple" => 100, "orange" => 150];
+$incomes = ["okozukai" => 10000, "partTimeSalary" => 35000];
+echo findTotalIncomes($incomes);
+
+function findTotalIncomes($incomes)
+{
+    $totalIncomes = 0;
+    foreach ($incomes as $income) {
+        $totalIncomes += $income;
+    }
+    return $totalIncomes;
+}
